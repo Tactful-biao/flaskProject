98fb0c754a461b449540618b0f59d3f80a80204e
Tactful-biao <1208339113@qq.com>
2021-06-22 11:14:43 +0800

    app v1.1 <username>

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-  return 'Hello World!'
+@app.route('/user/<name>')
+def hello_world(name):
+  return '<h1>Hello %s!</h1>' % name
 
 
 if __name__ == '__main__':
