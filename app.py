42d7628141acb1bcd02edef4a3c56d2621a80c31
Tactful-biao <1208339113@qq.com>
2021-06-22 13:44:28 +0800

    v1.4 render_template base.html extends

--- app.py
+++ app.py
@@ -1,34 +1,21 @@
-from flask import Flask, request, make_response, abort
-# from flask_script import Manager
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# manager = Manager(app)
 
 @app.route('/')
 def index():
-  user_agent = request.headers.get('User-Agent')
-  print(request.headers)
-  return 'Your Browser is %s!' % user_agent
+  return render_template("index.html")
+
+
+@app.route('/test')
+def test():
+  return render_template('test.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-  return '<h1>Hello %s!</h1>' % name
-
-
-@app.route('/status')
-def status():
-  response = make_response('This is response!')
-  response.set_cookie('answer', '42')
-  return response
-
-@app.route('/except/<id>')
-def exception(id):
-  print(id)
-  if id != '5':
-    abort(404)
-  return 'Hello %s' % id
+  return render_template("user.html", name=name)
 
 
 if __name__ == '__main__':
